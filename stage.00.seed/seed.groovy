48e069d4151561cdc992c4c405d2abdc2d928f03
ollin <oliver.nautsch@gmail.com>
2015-07-22 14:07:59 +0200

    seed.groovy changes

--- stage.00.seed/seed.groovy
+++ stage.00.seed/seed.groovy
@@ -1,4 +1,27 @@
-job('scpartner-jdsl-test'){
+def projectName = 'scpartner'
+
+
+job("${projectName}-01-compile"){
+    scm {
+        git {
+            remote {
+                github 'ollin/scpartner'
+            }
+            createTag false
+        }
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        gradle('build')
+    }
+    publishers {
+        buildPipelineTrigger("${projectName}-02-componenttest")
+    }
+}
+
+job("${projectName}-02-componenttest"){
     scm {
         git {
             remote {
